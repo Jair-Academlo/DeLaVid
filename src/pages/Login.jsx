1d4abd46a28377c2428c30064ded1d5054bc87f1
sasuske <jelver00@hotmail.com>
2023-11-15 09:29:30 -0500

    update changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,39 @@
+import '../styles/login.css';
+import { useState } from 'react';
+
 const Login = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 	return (
-		<div>
-			<h1>Aqui va el login</h1>
-		</div>
+		<>
+			<div className='container'>
+				<form>
+					<div>
+						<label htmlFor='email'>Email</label>
+						<input
+							type='text'
+							id='email'
+							placeholder='email'
+							value={email}
+							onChange={e => setEmail(e.target.value)}
+						/>
+					</div>
+					<div>
+						<label htmlFor='pass'>Email</label>
+						<input
+							type='password'
+							id='pass'
+							placeholder='password'
+							value={password}
+							onChange={e => setPassword(e.target.value)}
+						/>
+					</div>
+					<div>
+						<button>Iniciar Session</button>
+					</div>
+				</form>
+			</div>
+		</>
 	);
 };
 
