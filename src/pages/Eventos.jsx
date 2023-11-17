a12324b50799bac25917017e7683a90ebde3efc7
sasuske <jelver00@hotmail.com>
2023-11-17 10:03:43 -0500

    update changes

--- src/pages/Eventos.jsx
+++ src/pages/Eventos.jsx
@@ -1,10 +1,21 @@
+import N1 from '../imgs/logo.png';
 import '../styles/eventos.css';
 
 const Eventos = () => {
 	return (
 		<>
 			<div className='container-eventos-card'>
-				<h1>Eventos</h1>
+				<div>
+					<img src={N1} alt='imagen' width={100} height={100} />
+				</div>
+				<div>
+					<p>hola</p>
+					<p>{new Date().toLocaleDateString()}</p>
+				</div>
+				<div>
+					<button>editar</button>
+					<button>eliminar</button>
+				</div>
 			</div>
 		</>
 	);
