e0339f43ff10ba149cbd96b5e99c714e5866502a
sasuske <jelver00@hotmail.com>
2023-11-17 09:14:29 -0500

    update changes

--- src/pages/Notificaciones.jsx
+++ src/pages/Notificaciones.jsx
@@ -1,8 +1,32 @@
+import '../styles/notificaciones.css';
+import { IoIosNotifications } from 'react-icons/io';
+
 const Notificaciones = () => {
 	return (
-		<div>
-			<h1>Notificaciones</h1>
-		</div>
+		<>
+			<div className='container-notificaciones'>
+				<section>
+					<h2>Envia notificaciones a los usuarios</h2>
+					<p>Ingresa el mensaje que se le enviara a los usuarios</p>
+				</section>
+				<section>
+					<IoIosNotifications />
+					<input type='text' placeholder='Titulo' />
+				</section>
+				<section>
+					<textarea
+						name='mensaje'
+						id=''
+						cols='30'
+						rows='10'
+						placeholder='Mensaje'
+					></textarea>
+				</section>
+				<section>
+					<button>Enviar notificacion</button>
+				</section>
+			</div>
+		</>
 	);
 };
 
