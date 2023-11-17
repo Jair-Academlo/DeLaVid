eb82179971cf5273133c241e8907df2be61b2266
sasuske <jelver00@hotmail.com>
2023-11-17 10:19:24 -0500

    update changes

--- src/pages/Eventos.jsx
+++ src/pages/Eventos.jsx
@@ -5,16 +5,42 @@ const Eventos = () => {
 	return (
 		<>
 			<div className='container-eventos-card'>
-				<div>
-					<img src={N1} alt='imagen' width={100} height={100} />
+				<div className='div-eventos-imagen'>
+					<img src={N1} alt='imagen' />
 				</div>
-				<div>
-					<p>hola</p>
+				<div className='div-eventos-descripcion'>
+					<p>Este es un ejemplo de prueba en la descripcion.</p>
 					<p>{new Date().toLocaleDateString()}</p>
 				</div>
-				<div>
-					<button>editar</button>
-					<button>eliminar</button>
+				<div className='div-eventos-button'>
+					<button id='editar'>editar</button>
+					<button id='eliminar'>eliminar</button>
+				</div>
+			</div>
+			<div className='container-eventos-card'>
+				<div className='div-eventos-imagen'>
+					<img src={N1} alt='imagen' />
+				</div>
+				<div className='div-eventos-descripcion'>
+					<p>Este es un ejemplo de prueba en la descripcion.</p>
+					<p>{new Date().toLocaleDateString()}</p>
+				</div>
+				<div className='div-eventos-button'>
+					<button id='editar'>editar</button>
+					<button id='eliminar'>eliminar</button>
+				</div>
+			</div>
+			<div className='container-eventos-card'>
+				<div className='div-eventos-imagen'>
+					<img src={N1} alt='imagen' />
+				</div>
+				<div className='div-eventos-descripcion'>
+					<p>Este es un ejemplo de prueba en la descripcion.</p>
+					<p>{new Date().toLocaleDateString()}</p>
+				</div>
+				<div className='div-eventos-button'>
+					<button id='editar'>editar</button>
+					<button id='eliminar'>eliminar</button>
 				</div>
 			</div>
 		</>
