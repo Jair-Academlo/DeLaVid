3d692017840ade6e6ddc69a0af48e3882a06487b
sasuske <jelver00@hotmail.com>
2023-11-20 12:22:32 -0500

    update changes

--- src/components/crearEvento/CrearEvento.jsx
+++ src/components/crearEvento/CrearEvento.jsx
@@ -1,6 +1,16 @@
+import { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import { MdDateRange } from 'react-icons/md';
+import 'react-datepicker/dist/react-datepicker.css';
 import './crearEvento.css';
 
 const CrearEvento = () => {
+	const [selectedDate, setSelectedDate] = useState(null);
+
+	const handleDateChange = date => {
+		setSelectedDate(date);
+	};
+
 	return (
 		<>
 			<div className='container-modal'>
@@ -9,26 +19,36 @@ const CrearEvento = () => {
 						<h2>Crear Evento</h2>
 						<p>llena todos los campos para crear el evento</p>
 					</div>
-					<div>
-						<h2>Nombre del eventos</h2>
+					<div className='div-nombre-del-evento'>
+						<h2>Nombre del evento</h2>
 						<input
 							type='text'
 							placeholder='Escribe el nombre del evento'
 						/>
 					</div>
 					<div>
-						<h2>Fecha del evento</h2>
-						<span>Escribe la fecha del evento</span>
-						<input type='date' />
+						<h2>Fecha</h2>
+						<label htmlFor='date'>
+							Escribe la fecha del evento <MdDateRange />
+						</label>
+
+						<DatePicker
+							id='date'
+							selected={selectedDate}
+							onChange={handleDateChange}
+							dateFormat='yyyy-MM-dd'
+						/>
+
+						<p>
+							Fecha seleccionada:{' '}
+							{selectedDate &&
+								selectedDate.toISOString().split('T')[0]}
+						</p>
 					</div>
 					<div>
-						<h2>Hora del evento</h2>
+						<h2>Hora</h2>
 						<span>Coloca la hora del evento</span>
 						<input type='time' />
-					</div>
-					<div>
-						<span>imagen del evento</span>
-						<input type='file' />
 					</div>
 					<div>
 						<p>informacion:</p>
