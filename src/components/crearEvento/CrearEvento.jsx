6c06a5e58a35aad6e899001695021ac271558b27
sasuske <jelver00@hotmail.com>
2023-11-20 11:33:33 -0500

    update changes

--- src/components/crearEvento/CrearEvento.jsx
+++ src/components/crearEvento/CrearEvento.jsx
@@ -2,9 +2,47 @@ import './crearEvento.css';
 
 const CrearEvento = () => {
 	return (
-		<div>
-			<h1>aqui se va crear eventos</h1>
-		</div>
+		<>
+			<div className='container-modal'>
+				<div className='form-container-eventos'>
+					<div>
+						<h2>Crear Evento</h2>
+						<p>llena todos los campos para crear el evento</p>
+					</div>
+					<div>
+						<h2>Nombre del eventos</h2>
+						<input
+							type='text'
+							placeholder='Escribe el nombre del evento'
+						/>
+					</div>
+					<div>
+						<h2>Fecha del evento</h2>
+						<span>Escribe la fecha del evento</span>
+						<input type='date' />
+					</div>
+					<div>
+						<h2>Hora del evento</h2>
+						<span>Coloca la hora del evento</span>
+						<input type='time' />
+					</div>
+					<div>
+						<span>imagen del evento</span>
+						<input type='file' />
+					</div>
+					<div>
+						<p>informacion:</p>
+						<textarea
+							name='descripcion'
+							id='descripcion'
+							cols='20'
+							rows='5'
+							placeholder='Escribe la informacion del evento'
+						></textarea>
+					</div>
+				</div>
+			</div>
+		</>
 	);
 };
 
--- src/pages/Eventos.jsx
+++ src/pages/Eventos.jsx
@@ -4,98 +4,106 @@ import { BsFillTrashFill } from 'react-icons/bs';
 import { IoIosAddCircle } from 'react-icons/io';
 
 import '../styles/eventos.css';
+import { useState } from 'react';
+import CrearEvento from '../components/crearEvento/CrearEvento';
 
 const Eventos = () => {
+	const [modal, setModal] = useState(false);
+
 	return (
 		<>
-			<div>
-				<section>
-					<div className='container-eventos-card'>
-						<div className='div-eventos-imagen'>
-							<img src={N1} alt='imagen' />
+			{modal ? (
+				<CrearEvento />
+			) : (
+				<div>
+					<section>
+						<div className='container-eventos-card'>
+							<div className='div-eventos-imagen'>
+								<img src={N1} alt='imagen' />
+							</div>
+							<div className='div-eventos-descripcion'>
+								<p>
+									Este es un ejemplo de prueba en la
+									descripcion 1.
+								</p>
+								<p>{new Date().toLocaleDateString()}</p>
+							</div>
+							<div className='div-eventos-button'>
+								<button id='editar'>
+									Editar
+									<span>
+										<MdEdit />
+									</span>
+								</button>
+								<button id='eliminar'>
+									Eliminar{' '}
+									<span>
+										<BsFillTrashFill />{' '}
+									</span>
+								</button>
+							</div>
 						</div>
-						<div className='div-eventos-descripcion'>
-							<p>
-								Este es un ejemplo de prueba en la descripcion
-								1.
-							</p>
-							<p>{new Date().toLocaleDateString()}</p>
+						<div className='container-eventos-card'>
+							<div className='div-eventos-imagen'>
+								<img src={N1} alt='imagen' />
+							</div>
+							<div className='div-eventos-descripcion'>
+								<p>
+									Este es un ejemplo de prueba en la
+									descripcion 2.
+								</p>
+								<p>{new Date().toLocaleDateString()}</p>
+							</div>
+							<div className='div-eventos-button'>
+								<button id='editar'>
+									Editar{' '}
+									<span>
+										<MdEdit />
+									</span>
+								</button>
+								<button id='eliminar'>
+									Eliminar
+									<span>
+										<BsFillTrashFill />{' '}
+									</span>
+								</button>
+							</div>
 						</div>
-						<div className='div-eventos-button'>
-							<button id='editar'>
-								Editar
-								<span>
-									<MdEdit />
-								</span>
-							</button>
-							<button id='eliminar'>
-								Eliminar{' '}
-								<span>
-									<BsFillTrashFill />{' '}
-								</span>
-							</button>
+						<div className='container-eventos-card'>
+							<div className='div-eventos-imagen'>
+								<img src={N1} alt='imagen' />
+							</div>
+							<div className='div-eventos-descripcion'>
+								<p>
+									Este es un ejemplo de prueba en la
+									descripcion 3.
+								</p>
+								<p>{new Date().toLocaleDateString()}</p>
+							</div>
+							<div className='div-eventos-button'>
+								<button id='editar'>
+									Editar{' '}
+									<span>
+										<MdEdit />
+									</span>
+								</button>
+								<button id='eliminar'>
+									Eliminar{' '}
+									<span>
+										<BsFillTrashFill />{' '}
+									</span>
+								</button>
+							</div>
 						</div>
-					</div>
-					<div className='container-eventos-card'>
-						<div className='div-eventos-imagen'>
-							<img src={N1} alt='imagen' />
-						</div>
-						<div className='div-eventos-descripcion'>
-							<p>
-								Este es un ejemplo de prueba en la descripcion
-								2.
-							</p>
-							<p>{new Date().toLocaleDateString()}</p>
-						</div>
-						<div className='div-eventos-button'>
-							<button id='editar'>
-								Editar{' '}
-								<span>
-									<MdEdit />
-								</span>
-							</button>
-							<button id='eliminar'>
-								Eliminar
-								<span>
-									<BsFillTrashFill />{' '}
-								</span>
-							</button>
-						</div>
-					</div>
-					<div className='container-eventos-card'>
-						<div className='div-eventos-imagen'>
-							<img src={N1} alt='imagen' />
-						</div>
-						<div className='div-eventos-descripcion'>
-							<p>
-								Este es un ejemplo de prueba en la descripcion
-								3.
-							</p>
-							<p>{new Date().toLocaleDateString()}</p>
-						</div>
-						<div className='div-eventos-button'>
-							<button id='editar'>
-								Editar{' '}
-								<span>
-									<MdEdit />
-								</span>
-							</button>
-							<button id='eliminar'>
-								Eliminar{' '}
-								<span>
-									<BsFillTrashFill />{' '}
-								</span>
-							</button>
-						</div>
-					</div>
-				</section>
+					</section>
 
-				<section className='button-crear-evento'>
-					<button>
-						<IoIosAddCircle />
-					</button>
-				</section>
-			</div>
+					<section className='button-crear-evento'>
+						<button onClick={() => setModal(!modal)}>
+							<IoIosAddCircle />
+						</button>
+					</section>
+				</div>
+			)}
 		</>
 	);
 };
