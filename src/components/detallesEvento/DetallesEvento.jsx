8fcde7e749bea3981b47a7ba5e5a21ff933313cc
sasuske <jelver00@hotmail.com>
2023-11-27 08:30:37 -0500

    update changes

--- src/components/detallesEvento/DetallesEvento.jsx
+++ src/components/detallesEvento/DetallesEvento.jsx
@@ -1,11 +1,42 @@
+import './detallesEvento.css';
+import N1 from '../../imgs/logo.png';
+
 const DetallesEvento = () => {
 	return (
 		<>
-			<div>
-				<h1>aqui van los detalles del evento</h1>
+			<div className='container-ver-detalles-evento'>
+				<section className='section-form-ver-detalles-del-evento'>
+					<div>
+						<p>Nombre del evento</p>
+						<input type='text' value='Prueba 1' />
+					</div>
+					<div>
+						<p>Fecha</p>
+						<input type='text' value='2023-02-19' />
+					</div>
+					<div>
+						<p>Hora</p>
+						<input type='text' value='18:00' />
+					</div>
+					<div>
+						<p>Informacion</p>
+						<textarea name='' id='' cols='30' rows='10'></textarea>
+					</div>
+				</section>
+				<section className='section-img-ver-detalles-del-evento'>
+					<div>
+						<p>Imagen del evento</p>
+						<div>
+							<img
+								src={N1}
+								alt='imagen'
+								width={400}
+								height={400}
+							/>
+						</div>
+					</div>
+				</section>
 			</div>
-			<section></section>
-			<section></section>
 		</>
 	);
 };
