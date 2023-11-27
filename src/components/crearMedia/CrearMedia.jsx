0bef84efdc8d23b9dafd576b281140719c57938b
sasuske <jelver00@hotmail.com>
2023-11-27 11:02:01 -0500

    update changes

--- src/components/crearMedia/CrearMedia.jsx
+++ src/components/crearMedia/CrearMedia.jsx
@@ -1,27 +1,15 @@
-import { useState } from 'react';
-import { MdDateRange } from 'react-icons/md';
-import ReactDatePicker from 'react-datepicker';
 import './crearMedia.css';
 
 // eslint-disable-next-line react/prop-types
 const CrearMedia = ({ modal }) => {
-	const [selectedDate, setSelectedDate] = useState(null);
-
-	const handleDateChange = date => {
-		setSelectedDate(date);
-	};
-
 	return (
 		<>
 			<div className='container-modal-media'>
 				<div className='form-container-media'>
 					<section>
 						<div className='div-nombre-del-media'>
-							<h2>Nombre del Video</h2>
-							<input
-								type='text'
-								placeholder='Escribe el nombre del video'
-							/>
+							<h2>Titulo del video</h2>
+							<input type='text' placeholder='Titulo' />
 						</div>
 						<div className='div-buttom-del-media'>
 							<button id='cancelar' onClick={() => modal(false)}>
@@ -32,50 +20,34 @@ const CrearMedia = ({ modal }) => {
 							</button>
 						</div>
 					</section>
-					<div className='div-fecha-del-media'>
-						<h2>Fecha</h2>
-
-						<label htmlFor='date'>
-							Selecciona la fecha del video{' '}
-							<span>
-								{selectedDate &&
-									selectedDate.toISOString().split('T')[0]}
-							</span>{' '}
-							<MdDateRange />
-						</label>
-
-						<ReactDatePicker
-							id='date'
-							selected={selectedDate}
-							onChange={handleDateChange}
-							dateFormat='yyyy-MM-dd'
-						/>
-					</div>
-					<div className='div-hora-del-media'>
-						<h2>Hora</h2>
-						<div>
-							<label>
-								Coloca la hora del video{' '}
-								<input
-									type='time'
-									placeholder='Coloca la hora del video'
-								/>
-							</label>
-						</div>
-					</div>
-
-					<div className='div-file-del-media'>
-						<span>Imagen del video</span> <input type='file' />
+					<div className='div-nombre-del-media'>
+						<h2>Autor</h2>
+						<input type='text' placeholder='Autor' />
 					</div>
 					<div className='div-informacion-del-media'>
-						<h2>Informacion:</h2>
+						<h2>Descripcion:</h2>
 						<textarea
 							name='descripcion'
 							id='descripcion'
 							cols='70'
 							rows='5'
-							placeholder='Escribe la informacion del video'
+							placeholder='Descripcion del video'
 						></textarea>
+					</div>
+					<div className='div-nombre-del-media'>
+						<h2>Categoria</h2>
+						<input
+							type='text'
+							placeholder='Selecciona una categoria'
+						/>
+					</div>
+
+					<div className='div-file-del-media'>
+						<span>Imagen de Portada</span> <input type='file' />
+					</div>
+					<div className='div-nombre-del-media'>
+						<h2>Escribe la url del video</h2>
+						<input type='text' placeholder='URL del video' />
 					</div>
 				</div>
 			</div>
