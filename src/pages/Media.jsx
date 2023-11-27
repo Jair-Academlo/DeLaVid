adfb6027b1f98669ab782c5942ce3de85ed83fff
sasuske <jelver00@hotmail.com>
2023-11-27 09:01:01 -0500

    update changes

--- src/pages/Media.jsx
+++ src/pages/Media.jsx
@@ -1,8 +1,116 @@
+import { BsFillTrashFill } from 'react-icons/bs';
+import { IoIosAddCircle } from 'react-icons/io';
+import { MdEdit } from 'react-icons/md';
+import CrearEvento from '../components/crearEvento/CrearEvento';
+import { useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import N1 from '../imgs/logo.png';
+
 const Media = () => {
+	const { id = 1 } = useParams();
+	const navigate = useNavigate();
+	const [modal, setModal] = useState(false);
+
 	return (
-		<div>
-			<h1>Media</h1>
-		</div>
+		<>
+			{modal ? (
+				<CrearEvento modal={setModal} />
+			) : (
+				<div>
+					<section>
+						<div
+							className='container-eventos-card'
+							onClick={() => {
+								navigate(`/eventos/${id}`);
+							}}
+						>
+							<div className='div-eventos-imagen'>
+								<img src={N1} alt='imagen' />
+							</div>
+							<div className='div-eventos-descripcion'>
+								<p>
+									Este es un ejemplo de prueba en la
+									descripcion 1.
+								</p>
+								<p>{new Date().toLocaleDateString()}</p>
+							</div>
+							<div className='div-eventos-button'>
+								<button id='editar'>
+									Editar
+									<span>
+										<MdEdit />
+									</span>
+								</button>
+								<button id='eliminar'>
+									Eliminar{' '}
+									<span>
+										<BsFillTrashFill />{' '}
+									</span>
+								</button>
+							</div>
+						</div>
+						<div className='container-eventos-card'>
+							<div className='div-eventos-imagen'>
+								<img src={N1} alt='imagen' />
+							</div>
+							<div className='div-eventos-descripcion'>
+								<p>
+									Este es un ejemplo de prueba en la
+									descripcion 2.
+								</p>
+								<p>{new Date().toLocaleDateString()}</p>
+							</div>
+							<div className='div-eventos-button'>
+								<button id='editar'>
+									Editar{' '}
+									<span>
+										<MdEdit />
+									</span>
+								</button>
+								<button id='eliminar'>
+									Eliminar
+									<span>
+										<BsFillTrashFill />{' '}
+									</span>
+								</button>
+							</div>
+						</div>
+						<div className='container-eventos-card'>
+							<div className='div-eventos-imagen'>
+								<img src={N1} alt='imagen' />
+							</div>
+							<div className='div-eventos-descripcion'>
+								<p>
+									Este es un ejemplo de prueba en la
+									descripcion 3.
+								</p>
+								<p>{new Date().toLocaleDateString()}</p>
+							</div>
+							<div className='div-eventos-button'>
+								<button id='editar'>
+									Editar{' '}
+									<span>
+										<MdEdit />
+									</span>
+								</button>
+								<button id='eliminar'>
+									Eliminar{' '}
+									<span>
+										<BsFillTrashFill />{' '}
+									</span>
+								</button>
+							</div>
+						</div>
+					</section>
+
+					<section className='button-crear-evento'>
+						<button onClick={() => setModal(!modal)}>
+							<IoIosAddCircle />
+						</button>
+					</section>
+				</div>
+			)}
+		</>
 	);
 };
 
