8d467bc0a28cd3f793ebb96cfea4cbc5ffef0fdd
sasuske <jelver00@hotmail.com>
2023-12-22 08:46:05 -0500

    update changes

--- src/pages/Media.jsx
+++ src/pages/Media.jsx
@@ -1,16 +1,66 @@
+import CrearMedia from '../components/crearMedia/CrearMedia';
+import { getDatabase, ref, child, get, remove } from 'firebase/database';
+import { database } from '../config/firebase';
 import { BsFillTrashFill } from 'react-icons/bs';
 import { IoIosAddCircle } from 'react-icons/io';
 import { MdEdit } from 'react-icons/md';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import CrearMedia from '../components/crearMedia/CrearMedia';
+import { setData } from '../store/data/dataSlice';
+import { setEditar } from '../store/data/editarSlice';
+import { useDispatch } from 'react-redux';
 import N1 from '../imgs/logo.png';
 import '../styles/media.css';
 
 const Media = () => {
+	const [mediaVideoData, setMediaVideoData] = useState([]);
 	const { id = 1 } = useParams();
 	const navigate = useNavigate();
 	const [modal, setModal] = useState(false);
+
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const dbRef = ref(getDatabase());
+				const snapshot = await get(
+					child(
+						dbRef,
+						'projects/proj_cer3wPMCkxSWWePnENPiZL/data/Media'
+					)
+				);
+
+				if (snapshot.exists()) {
+					const data = snapshot.val();
+					const dataArray = Object.entries(data).map(
+						([key, value], index) => ({
+							serial: index,
+							id: key,
+							...value,
+						})
+					);
+					setMediaVideoData(dataArray);
+				} else {
+					console.log('No hay datos en la colección "Media"');
+				}
+			} catch (error) {
+				console.error('Error al obtener los datos:', error);
+			}
+		};
+
+		fetchData();
+	}, [modal]);
+
+	const editarMediaVideo = data => {
+		setModal(!modal);
+		dispatch(setData(data));
+		dispatch(setEditar(true));
+	};
+
+	const dirigirDetallesMediaVideo = data => {
+		dispatch(setData(data));
+	};
 
 	return (
 		<>
