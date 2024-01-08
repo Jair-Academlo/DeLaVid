4bb82bcbff6262b8e821cccdc3e3907815d04a78
sasuske <jelver00@hotmail.com>
2024-01-08 08:49:18 -0500

    update changes

--- src/components/crearMedia/CrearMedia.jsx
+++ src/components/crearMedia/CrearMedia.jsx
@@ -1,7 +1,25 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { getStorage } from 'firebase/storage';
+import { app } from '../../config/firebase';
+import { useState } from 'react';
 import './crearMedia.css';
 
 // eslint-disable-next-line react/prop-types
 const CrearMedia = ({ modal }) => {
+	const data = useSelector(state => state.data);
+	const editar = useSelector(state => state.editar);
+	const dispatch = useDispatch();
+	const storage = getStorage(app);
+
+	const [titulo, setTitulo] = useState('');
+	const [autor, setAutor] = useState('');
+	const [descripcion, setDescripcion] = useState('');
+	const [image, setImage] = useState(null);
+	const [file, setFile] = useState(null);
+	const [urlVideo, setUrlVideo] = useState('');
+	const [loadding, setLoadding] = useState(false);
+
+	console.log(data);
 	return (
 		<>
 			<div className='container-modal-media'>
@@ -9,7 +27,14 @@ const CrearMedia = ({ modal }) => {
 					<section>
 						<div className='div-nombre-del-media'>
 							<h2>Titulo del video</h2>
-							<input type='text' placeholder='Titulo' />
+							<input
+								type='text'
+								placeholder='Titulo'
+								defaultValue={titulo}
+								onChange={e => {
+									setTitulo(e.target.value);
+								}}
+							/>
 						</div>
 						<div className='div-buttom-del-media'>
 							<button id='cancelar' onClick={() => modal(false)}>
@@ -22,7 +47,14 @@ const CrearMedia = ({ modal }) => {
 					</section>
 					<div className='div-nombre-del-media'>
 						<h2>Autor</h2>
-						<input type='text' placeholder='Autor' />
+						<input
+							type='text'
+							placeholder='Autor'
+							defaultValue={autor}
+							onChange={e => {
+								setAutor(e.target.value);
+							}}
+						/>
 					</div>
 					<div className='div-informacion-del-media'>
 						<h2>Descripcion:</h2>
@@ -32,6 +64,10 @@ const CrearMedia = ({ modal }) => {
 							cols='70'
 							rows='5'
 							placeholder='Descripcion del video'
+							defaultValue={descripcion}
+							onChange={e => {
+								setDescripcion(e.target.value);
+							}}
 						></textarea>
 					</div>
 					<div className='div-nombre-del-media'>
@@ -40,6 +76,7 @@ const CrearMedia = ({ modal }) => {
 							type='text'
 							placeholder='Selecciona una categoria'
 							value='Devocionales'
+							readOnly
 						/>
 					</div>
 
@@ -48,7 +85,14 @@ const CrearMedia = ({ modal }) => {
 					</div>
 					<div className='div-nombre-del-media'>
 						<h2>Escribe la url del video</h2>
-						<input type='text' placeholder='URL del video' />
+						<input
+							type='text'
+							placeholder='URL del video'
+							defaultValue={urlVideo}
+							onChange={e => {
+								setUrlVideo(e.target.value);
+							}}
+						/>
 					</div>
 				</div>
 			</div>
