388da5d5db9bd087103aa39ea764e894471c74a0
sasuske <jelver00@hotmail.com>
2024-01-10 09:56:39 -0500

    update changes

--- src/components/crearMedia/CrearMedia.jsx
+++ src/components/crearMedia/CrearMedia.jsx
@@ -3,6 +3,7 @@ import { getStorage } from 'firebase/storage';
 import { app } from '../../config/firebase';
 import { useEffect, useState } from 'react';
 import './crearMedia.css';
+import IsLoadding from '../isLoadding/IsLoadding';
 
 // eslint-disable-next-line react/prop-types
 const CrearMedia = ({ modal }) => {
@@ -39,79 +40,89 @@ const CrearMedia = ({ modal }) => {
 	console.log(data);
 	return (
 		<>
+			{loadding && <IsLoadding />}
+
 			<div className='container-modal-media'>
-				<div className='form-container-media'>
-					<section>
-						<div className='div-nombre-del-media'>
-							<h2>Titulo del video</h2>
+				<form
+					className='form-container-media' /* onSubmit={newEvent} */
+				>
+					<section className='section-1-form-media'>
+						<div className='div-nombre-del-evento'>
+							<h2>Autor</h2>
 							<input
 								type='text'
-								placeholder='Titulo'
-								defaultValue={titulo}
+								placeholder='Autor'
+								defaultValue={autor}
 								onChange={e => {
-									setTitulo(e.target.value);
+									setAutor(e.target.value);
 								}}
 							/>
 						</div>
+						<div className='div-informacion-del-media'>
+							<h2>Descripcion:</h2>
+							<textarea
+								name='descripcion'
+								id='descripcion'
+								cols='70'
+								rows='5'
+								placeholder='Descripcion del video'
+								defaultValue={descripcion}
+								onChange={e => {
+									setDescripcion(e.target.value);
+								}}
+							></textarea>
+						</div>
+						<div className='div-nombre-del-media'>
+							<h2>Categoria</h2>
+							<input
+								type='text'
+								placeholder='Selecciona una categoria'
+								value='Devocionales'
+								readOnly
+							/>
+						</div>
+
+						<div className='div-file-del-media'>
+							<span>Imagen de Portada</span> <input type='file' />
+						</div>
+						<div className='div-nombre-del-media'>
+							<h2>Escribe la url del video</h2>
+							<input
+								type='text'
+								placeholder='URL del video'
+								defaultValue={urlVideo}
+								onChange={e => {
+									setUrlVideo(e.target.value);
+								}}
+							/>
+						</div>
+					</section>
+					<section className='section-2-form-media'>
 						<div className='div-buttom-del-media'>
-							<button id='cancelar' onClick={() => modal(false)}>
+							<button /* onClick={cancelar} */
+								id='cancelar-click-media'
+							>
 								Cancelar
 							</button>
-							<button onClick={() => modal(false)}>
+							<button
+								type='submit'
+								onClick={() => setLoadding(true)}
+							>
 								Guardar
 							</button>
 						</div>
+						<div>
+							<img
+								src={
+									editar
+										? image || data.imagen
+										: image || 'iconImagen'
+								}
+								alt='imagen'
+							/>
+						</div>
 					</section>
-					<div className='div-nombre-del-media'>
-						<h2>Autor</h2>
-						<input
-							type='text'
-							placeholder='Autor'
-							defaultValue={autor}
-							onChange={e => {
-								setAutor(e.target.value);
-							}}
-						/>
-					</div>
-					<div className='div-informacion-del-media'>
-						<h2>Descripcion:</h2>
-						<textarea
-							name='descripcion'
-							id='descripcion'
-							cols='70'
-							rows='5'
-							placeholder='Descripcion del video'
-							defaultValue={descripcion}
-							onChange={e => {
-								setDescripcion(e.target.value);
-							}}
-						></textarea>
-					</div>
-					<div className='div-nombre-del-media'>
-						<h2>Categoria</h2>
-						<input
-							type='text'
-							placeholder='Selecciona una categoria'
-							value='Devocionales'
-							readOnly
-						/>
-					</div>
-
-					<div className='div-file-del-media'>
-						<span>Imagen de Portada</span> <input type='file' />
-					</div>
-					<div className='div-nombre-del-media'>
-						<h2>Escribe la url del video</h2>
-						<input
-							type='text'
-							placeholder='URL del video'
-							defaultValue={urlVideo}
-							onChange={e => {
-								setUrlVideo(e.target.value);
-							}}
-						/>
-					</div>
-				</div>
+				</form>
 			</div>
 		</>
 	);
